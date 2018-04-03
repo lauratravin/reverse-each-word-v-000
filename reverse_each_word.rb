73adb1e7d2a22105e8e3c52572073fd0d12d92c2
Laura Messa Travin <laura.travin@gmail.com>
2018-04-03 05:35:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,10 @@ def reverse_each_word(sentence1)
    array = []
    new_array = []
    array << sentence1.split(" ")
-  #  array.each {|text|
-  #    new_array <<   text.reverse
-  #     }
-    array.reverse
-    # new_array
+    array.each {|text|
+      new_array <<   text.reverse
+      put text.reverse
+       }
+
+   new_array
 end
