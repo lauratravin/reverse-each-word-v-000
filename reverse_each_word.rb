e061f665804e7317c6bcc0b9a1eb310ebe0522e5
Laura Messa Travin <laura.travin@gmail.com>
2018-04-03 06:13:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(sentence1)
 
        string1 = text.join.reverse  #tranfor each in string
        return string1
-
+       new_array << string1.split(" ")
       #  new_array << string1 # assign the reverse text to the new array
       #    new_array  #return new array
        }
